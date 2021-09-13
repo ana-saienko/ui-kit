3dd0ed0c23eae0d0483530d3328fe41ee9d9b616
ana-saienko <saienko_ana@icloud.com>
2021-09-13 12:23:34 +0200

    did a lot, download and progress, and input range. left for tomorrow

--- js/script.js
+++ js/script.js
@@ -84,3 +84,24 @@ noUiSlider.create(slider, {
 		 },
 	 }
 });
+
+const progress = document.querySelector('.progress-done');
+
+progress.style.width = progress.getAttribute('data-done') + '%';
+progress.style.opacity = 1;
+
+// -----------------------------------
+
+const circle = document.querySelector('.progress-ring__circle');
+const radius = circle.r.baseVal.value;
+const circumference = 2 * Math.PI * radius;
+
+circle.style.strokeDasharray = `${circumference} ${circumference}`;
+circle.style.strokeDashoffset = circumference;
+
+function setProgress(percent) {
+    const offset = circumference - percent / 100 * circumference;
+    circle.style.strokeDashoffset = offset;
+}
+
+setProgress(69);
